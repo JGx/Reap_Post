6fa3803d6b26e896aea53fa75c3b46ee00c97280
Jonathan Gilad <giladster@gmail.com>
2014-11-11 17:31:36 -0500

    Added arg parser

--- scraper.py
+++ scraper.py
@@ -1,7 +1,17 @@
 import praw
+import argparse
+
+#Parser asks user for subreddit to pull from
+parser = argparse.ArgumentParser(description='Reap Post')
+parser.add_argument('-S', dest='subreddit',  required=True)
+
+p = vars(parser.parse_args())
+subreddit = p['subreddit']
+
+print subreddit
 
 r = praw.Reddit(user_agent="reappost")
-submissions = r.get_subreddit('funny').get_hot(limit=25)
+submissions = r.get_subreddit(subreddit).get_hot(limit=25)
 
 for x in submissions:
 	print str(x)
