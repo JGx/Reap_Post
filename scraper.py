dbdd3f3bcba6687990c607578d392724689e772c
Connor Taylor <conbond93@gmail.com>
2014-11-11 17:41:47 -0500

    added Post class

--- scraper.py
+++ scraper.py
@@ -6,10 +6,21 @@ class Scraper:
 		self.r = praw.Reddit(user_agent="reappost")
 		return
 
+	# Scrapes the given subreddit for the latest 25 hot posts
 	def scrape(self):
 		submissions = self.r.get_subreddit('pics').get_hot(limit=25)
 		for x in submissions:
 			print x.url
+
+class Post:
+
+	def __init__(self):
+		return
+
+	# prints the info about this post
+	def info():
+		print "Will add more info later"
+
 
 scraper = Scraper()
 scraper.scrape()
