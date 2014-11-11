b6209984f1f71bb426688384776c68d8912546ea
Connor Taylor <conbond93@gmail.com>
2014-11-11 17:30:10 -0500

    basic scraping class

--- scraper.py
+++ scraper.py
@@ -1,7 +1,15 @@
 import praw
 
-r = praw.Reddit(user_agent="reappost")
-submissions = r.get_subreddit('funny').get_hot(limit=25)
+class Scraper:
 
-for x in submissions:
-	print str(x)
+	def __init__(self):
+		self.r = praw.Reddit(user_agent="reappost")
+		return
+
+	def scrape(self):
+		submissions = self.r.get_subreddit('pics').get_hot(limit=25)
+		for x in submissions:
+			print x.url
+
+scraper = Scraper()
+scraper.scrape()
