1fbd21cdb8bacb7393e1e078698bcd9ea70de885
Jonathan Gilad <giladster@gmail.com>
2014-11-19 15:18:33 -0500

    Queue sending mechanism written

--- imageQueue.py
+++ imageQueue.py
@@ -8,18 +8,18 @@ class ImageQueue:
 		self.imgLink = origImgLink
 		self.queue = Queue.LifoQueue()
 		self.mainPipe = mainPipe
-		self.workerWorking = []
+		self.pipeBusy = []
 		#create numAgents pipes
 		self.numAgents = maxNumAgents
 		self.processPipes = []
 		for i in xrange(0,maxNumAgents):
 			pipein, pipeout = Pipe()
 			self.processPipes.append([pipein,pipeout])
-			self.workerWorking[i] = False
+			self.pipeBusy[i] = False
 		#initialize processes and pass the pipe
 		for i in xrange(0,maxNumAgents):
 			p = Process(target=mkNewImgMatcher,
-						args= (self.processPipes[i][1],self.imgLink))
+						args= (i,self.processPipes[i][1],self.imgLink))
 			p.start()
 			#p.join()
 		return
@@ -31,24 +31,51 @@ class ImageQueue:
 	def pop(self):
 		return self.queue.get()
 
+	def queueEmpty(self):
+		return self.queue.empty()
+
 	def printElems(self):
 		for elem in iter(self.queue.get,None):
 			print elem
+
+	def pollAllPipes(self):
+		for [pipeIn,pipeOut] in self.processPipes:
+			gotMail = pipeOut.poll()
+			if gotMail != None:
+				return gotMail
+
+	def sendToArbitraryPipe(self):
+		#check if queue empty
+		if self.queueEmpty() == False:
+			#if not empty, pop and send to first available pipe
+			for i in xrange(0,len(self.processPipes.length)):
+				if self.pipeBusy[i] == False:
+					self.sendToPipe(i)
+					return
+
+
+	def sendToPipe(self,pid):
+		self.pipeBusy[i] = True
+		self.processPipes[i].send("NEWIMG",self.pop())
+
+	#first check if queue empty
+	def checkQueueAndSendToPipe(self,pid):
+		if self.queueEmpty() == False:
+			self.sendToPipe(pid)
 
 	#receive from Reddit Scraper and from ImageProcessorWorkers
 	def receiveLoop(self):
 		try:
 			while True:
-				(pid,msg) = self.recv()
-				if pid==0:#Reddit Scraper
+				(pid,msg) = self.pollAllPipes()
+				#check queue and send to workers appropriately
+				sendToArbitraryPipe()
+				if pid == 0:#Reddit Scraper
 					self.push(msg)
-				elif msg=="NEWIMG":
-					self.processPipes[pid].send(self.pop())
+				elif msg == "NEWIMG":
+					self.sendToPipe(pid)
 		except IOError:
 			print 'IOError'
-		self.push(img)
 
 #test image queue
-imgQ = ImageQueue(5)
-imgQ.push("hey")
-imgQ.printElems()
+
