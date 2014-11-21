4d1bc900864443e6f12183b1b2b1e96709d568ea
unknown <brian.cefali@tufts.edu>
2014-11-21 16:39:56 -0500

    preping networking

--- imgmatcher.py
+++ imgmatcher.py
@@ -1,17 +1,20 @@
 from PIL import Image
 import urllib, cStringIO
 from multiprocessing import Process, Pipe
+import scraper
+import requests
 
-def mkNewImgMatcher(pid, queuePipe, originalImage):
-	matcher = ImgMatcher(pid, queuePipe, originalImage)
+def mkNewImgMatcher(pid, queuePipe, originalImage, threshold=0.95):
+	matcher = ImgMatcher(pid, queuePipe, originalImage, threshold)
 	matcher.run()
 
 class ImgMatcher:
 	
-	def __init__(self, pid, queuePipe, origImg):
+	def __init__(self, pid, queuePipe, origImg, threshold):
 		self.queuePipe = queuePipe
 		self.origImg = origImg
 		self.pid = pid
+		self.threshold = threshold
 		
 	def run(self):
 		try:
@@ -22,32 +25,27 @@ class ImgMatcher:
 					self.queuePipe.close()
 					return
 				elif msgType == 'NEWIMG':
-					img = Image.open(cStringIO.StringIO(urllib.urlopen(msg).read()))
+					img = Image.open(cStringIO.StringIO(urllib.urlopen(msg.url).read()))
 					imgdata = list(img.getdata())
 					if len(self.origImg) != len(imgdata):
-						print("Images did NOT match")
+						sendResult(False, msg)
 					else:
-						match = True
-						for i in range(0,len(imgdata)):
-							(ra,ga,ba) = self.origImg[i]
-							(rb,gb,bb) = imgdata[i]
-							if ra != rb or ga != gb or ba != bb:
-								print("Images did NOT match")
-								match = False
-								break
-						if match:
-							print("Images did INDEED match")
+						similarity = compareImage(imgdata)
+						if similarity >= self.threshold:
+							sendResult(True, msg)
 		except IOError as e:
 			print("I/O error({0}): {1}".format(e.errno, e.strerror))
 	
-def start():
-	orig = Image.open(cStringIO.StringIO(urllib.urlopen("http://i.imgur.com/dMfco99.jpg").read()))
-	myPipe, yourPipe = Pipe()
-	match = Process(target=mkNewImgMatcher, args=(yourPipe, list(orig.getdata())))
-	match.start()
-	myPipe.send(("NEWIMG", "http://i.imgur.com/x4ZxyBQ.jpg"))
-	myPipe.send(("HALT", None))
-	match.join()
-
-if __name__ == '__main__':    
-    start()
+	def compareImage(otherImg):
+		datalen = len(otherImg)
+		for i in range(0,datalen):
+			(ra,ga,ba) = self.origImg[i]
+			(rb,gb,bb) = imgdata[i]
+			if ra != rb or ga != gb or ba != bb:
+				print("Images did NOT match")
+				match = False
+				break
+				
+	def sendResult(match, msg):
+		return requests.get('', params={'url':msg.url, 'score':msg.score, 'title':msg.title, 'num_comments':msg.num_comments, 'match':match})
+		
